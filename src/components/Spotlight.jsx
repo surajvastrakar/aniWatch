1bba1e696a0b93cba54c027c241f252371af2f08
Suraj <surajvastrakar007@gmail.com>
2024-03-08 14:23:23 +0530

    router issue fixed

--- src/components/Spotlight.jsx
+++ src/components/Spotlight.jsx
@@ -1,21 +1,20 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-import "../../src/assets/style.css"
-
+import "../../src/assets/style.css";
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
-const Spotlight = ({spotlights}) => {
+const Spotlight = ({ spotlights }) => {
   return (
-    <div className=''>
+    <div className="">
       <Swiper
         spaceBetween={30}
         autoplay={{
@@ -29,40 +28,58 @@ const Spotlight = ({spotlights}) => {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        {
-            spotlights.map((spotlight) => (
-                <div className="">
-                    <SwiperSlide className='text-white'>
-                        <div className='h-[600px] bg-cover w-full bg-no-repeat relative' style={{backgroundImage:`url(${spotlight.poster})`}}>
-                            <div className='left-0 text-left px-20 absolute inset-0 bg-[#00000097]'>
-                                <div className='w-1/2'>
-                                    <p className='text-[#FFDD95] text-lg my-6'>#{spotlight.rank} Spotlight</p>
-                                    <h2 className='text-5xl my-6 font-bold'>{spotlight.name}</h2>
-                                    <div className='flex gap-4 my-6 font-medium'>
-                                        {
-                                            spotlight.otherInfo.map((info, i) => (
-                                                <p className={i == spotlight.otherInfo.length - 1 ? 'bg-green-400 border rounded px-2 text-black' : ''}>
-                                                    {info}
-                                                </p>
-                                            ))
-                                        }
-                                    </div>
-                                    <article className='my-6'>{spotlight.description.length > 250 ? `${spotlight.description.substring(0, 250)}...` : spotlight.description}</article>
-                                    <div className=''>
-                                        <button className='mr-5 bg-[#ffdd95] px-4 py-2 border border-[#ffdd95] rounded-full text-black '>Watch Now</button>
-                                        <button className='ml-5 bg-[#56565b] px-4 py-2 border border-[#56565b] rounded-full text-white'>Details</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* <img src={spotlight.poster} alt="poster" /> */}
-                    </SwiperSlide>
+        {spotlights.map((spotlight) => (
+          <div className="" key={spotlight.name}>
+            <SwiperSlide className="text-white" key={spotlight.name}>
+              <div
+                className="h-[600px] bg-cover w-full bg-no-repeat relative"
+                style={{ backgroundImage: `url(${spotlight.poster})` }}
+              >
+                <div className="left-0 text-left px-20 absolute inset-0 bg-[#00000097]">
+                  <div className="w-1/2">
+                    <p className="text-[#FFDD95] text-lg my-6">
+                      #{spotlight.rank} Spotlight
+                    </p>
+                    <h2 className="text-5xl my-6 font-bold">
+                      {spotlight.name}
+                    </h2>
+                    <div className="flex gap-4 my-6 font-medium">
+                      {spotlight.otherInfo.map((info, i) => (
+                        <p
+                          key={`${i}-key`}
+                          className={
+                            i == spotlight.otherInfo.length - 1
+                              ? "bg-green-400 border rounded px-2 text-black"
+                              : ""
+                          }
+                        >
+                          {info}
+                        </p>
+                      ))}
+                    </div>
+                    <article className="my-6">
+                      {spotlight.description.length > 250
+                        ? `${spotlight.description.substring(0, 250)}...`
+                        : spotlight.description}
+                    </article>
+                    <div className="">
+                      <button className="mr-5 bg-[#ffdd95] px-4 py-2 border border-[#ffdd95] rounded-full text-black ">
+                        Watch Now
+                      </button>
+                      <button className="ml-5 bg-[#56565b] px-4 py-2 border border-[#56565b] rounded-full text-white">
+                        Details
+                      </button>
+                    </div>
+                  </div>
                 </div>
-            ))
-        }
+              </div>
+              {/* <img src={spotlight.poster} alt="poster" /> */}
+            </SwiperSlide>
+          </div>
+        ))}
       </Swiper>
     </div>
   );
-}
+};
 
-export default Spotlight
+export default Spotlight;
--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,26 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const Trending = ({trendingData}) => {
+const Trending = ({ trendingData }) => {
   return (
-    <div className='mt-16'>
-        <h2 className='text-[#FFDD95] text-2xl font-bold ml-4 mb-12'>Trending</h2>
-        <div className='flex gap-6'>
-            {
-                trendingData.map((trending) => (
-                    <div className='flex'> 
-                        <div className='h-[150px] items-center relative w-9'>
-                            <h3 className='rotate-180 overflow-hidden absolute bottom-8 text-nowrap' style={{writingMode: 'vertical-lr'}}>{trending.name.length > 10 ? `${trending.name.substring(0, 10)}...` : trending.name}</h3>
-                            <p className='text-[#FFDD95] text-lg font-bold absolute bottom-0 m-auto'>{trending.rank < 9 ? 0 : ''}{trending.rank}</p>
-                        </div>
-                        <div>
-                            <img src={trending.poster} alt="" className='h-[150px]'  />
-                        </div>
-                    </div>
-                ))
-            }
-        </div>
+    <div className="mt-16">
+      <h2 className="text-[#FFDD95] text-2xl font-bold ml-4 mb-12">Trending</h2>
+      <div className="flex gap-6">
+        {trendingData.map((trending) => (
+          <div className="flex" key={trending.name}>
+            <div className="h-[150px] items-center relative w-9">
+              <h3
+                className="rotate-180 overflow-hidden absolute bottom-8 text-nowrap"
+                style={{ writingMode: "vertical-lr" }}
+              >
+                {trending.name.length > 10
+                  ? `${trending.name.substring(0, 10)}...`
+                  : trending.name}
+              </h3>
+              <p className="text-[#FFDD95] text-lg font-bold absolute bottom-0 m-auto">
+                {trending.rank < 9 ? 0 : ""}
+                {trending.rank}
+              </p>
+            </div>
+            <div>
+              <img src={trending.poster} alt="" className="h-[150px]" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Trending
+export default Trending;
